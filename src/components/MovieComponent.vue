18c952eb508f58c5b4eadaa8da702ac20fb1d6e1
jacoposergio <djago32@gmail.com>
2022-07-13 15:06:05 +0200

    loaded flag

--- src/components/MovieComponent.vue
+++ src/components/MovieComponent.vue
@@ -2,22 +2,41 @@
     <li>
         <div>Titolo:{{item.title}}</div>
         <div>Titolo originale: {{item.original_title}}</div>
-        <div>Lingua: {{item.original_language}}</div>
+         <div >
+          <img 
+            v-if="`https://countryflagsapi.com/png/${item.original_language}`.loaded === false"
+            :src="`https://countryflagsapi.com/png/${item.original_language}`" 
+            alt="language"
+            @load="`https://countryflagsapi.com/png/${item.original_language}`.loaded = true"
+           >
+            <img 
+            v-else
+            src="https://countryflagsapi.com/png/br" 
+            alt="language"
+           >
+        </div>
+        <!-- v-if="item.original_language === 'it'">Lingua: {{item.original_language}}</div> -->
         <div>Voto: {{item.vote_average}}</div>
     </li>
 </template>
 
 <script>
+
 export default {
     name: 'MovieComponent',
     data: function () {
       return{
-
+         isLoaded: false
       };
     },
     props:{
        "item": Object
-    } 
+    }, 
+     methods: {
+          onImgLoad () {
+          this.isLoaded = true
+        }
+     },
 }
 
 </script>
